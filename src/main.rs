d52634408e03253888e94630dd821973aba84688
Karl Joyeux <newdracoaccount@proton.me>
2026-06-05 17:37:48 +0200

    designing 1 place

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Place {
+    id: &str, 
+    behaviour: &str,
+    particle: u8, 
+
+} 
+
+impl Place {
+    fn new(behaviour: &str) -> Place {
+        Place{ id: "place", behaviour: "neutral", particle: 0 }
+    }
+
+    fn set_particle(&mut self, n: u8) {
+        self.particle = n; 
+    }
+    
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
